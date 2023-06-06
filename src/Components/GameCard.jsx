cdf1c6fa477acac374872c07f182cad7992d6803
CARR0T02 <mr.gabriel.chang@gmail.com>
2023-06-06 11:30:07 +0800

    Implement link from home to gamepage

--- src/Components/GameCard.jsx
+++ src/Components/GameCard.jsx
@@ -1,14 +1,16 @@
 import '../styles/GameCard.css';
 import { Card } from 'flowbite-react';
 import React from 'react';
-import imageURL from './mock/mockAssets/pierre-roussel-dreamcast-phone2-min.jpg';
+import { Link } from 'react-router-dom';
 
 export default function GameCard({ docData }) {
   const name = docData.levelName;
-  // const imageURL = docData.levelURL;
+  const imageURL = docData.levelURL;
   return (
-    <Card imgSrc={imageURL} className='h-96'>
-      <h4 className='text-xl text-center'>{name}</h4>
-    </Card>
+    <Link to='game' state={{ docData: docData }}>
+      <Card key={name} imgSrc={imageURL} className='h-96'>
+        <h4 className='text-xl text-center'>{name}</h4>
+      </Card>
+    </Link>
   );
 }
--- src/Components/GamePage.jsx
+++ src/Components/GamePage.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import ErrorPage from './ErrorPage';
+import { useLocation } from 'react-router-dom';
+import { Sidebar } from 'flowbite-react';
 
-export function Component({ docData }) {
-  return <div>GamePage</div>;
+export function Component() {
+  let state = useLocation();
+  const docData = state.state.docData;
+
+  return (
+    <div id='game-page'>
+      <Sidebar>
+        <Sidebar.Items>
+          <Sidebar.ItemGroup>
+            <Sidebar.Item>Char 1</Sidebar.Item>
+            <Sidebar.Item>Char 2</Sidebar.Item>
+            <Sidebar.Item>Char 3</Sidebar.Item>
+          </Sidebar.ItemGroup>
+        </Sidebar.Items>
+      </Sidebar>
+      <div id='image-container'>
+        <img src={docData?.levelURL} alt={docData?.levelName} />
+      </div>
+    </div>
+  );
 }
 
 export function ErrorBoundary() {
