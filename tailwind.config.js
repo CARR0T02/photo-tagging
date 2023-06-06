d3637d07bbbabd3f9ab8229b5b46632879187a24
CARR0T02 <mr.gabriel.chang@gmail.com>
2023-06-06 14:49:36 +0800

    Add custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: {
+          light: '#638bbb',
+          DEFAULT: '#3e5775',
+          dark: '#19232f',
+        },
+        primary: '#Dc6053',
+        secondary: '#9B59B6',
+      },
+    },
   },
   plugins: [require('flowbite/plugin')],
 };
