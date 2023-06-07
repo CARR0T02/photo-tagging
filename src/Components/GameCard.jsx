620528f1dfa7c7496f6c2782bd472d6a5f10a0be
CARR0T02 <mr.gabriel.chang@gmail.com>
2023-06-07 10:38:41 +0800

    Implement fetch imageURL from cloud

--- src/Components/GameCard.jsx
+++ src/Components/GameCard.jsx
@@ -2,12 +2,16 @@ import '../styles/GameCard.css';
 import { Card } from 'flowbite-react';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDownloadURL } from 'react-firebase-hooks/storage';
+import { ref as storageRef } from 'firebase/storage';
+import { storage } from '../firebase-config';
 
 export default function GameCard({ docData, className }) {
+  // const [imageURL] = useDownloadURL(storageRef(storage, docData.levelURL));
   const name = docData.levelName;
   const imageURL = docData.levelURL;
   return (
-    <Link to='game' state={{ docData: docData }}>
+    <Link to='game' state={{ imageURL: imageURL, docData: docData }}>
       <Card key={name} imgSrc={imageURL} className={`${className} `}>
         <h4 className='text-primary text-xl text-center'>{name}</h4>
       </Card>
