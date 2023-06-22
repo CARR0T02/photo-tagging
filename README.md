57c38b4a0bef9bf41602674c9c40d6405df7686f
GabrielCWT <mr.gabriel.chang@gmail.com>
2023-06-22 11:54:01 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Photo Tagging App
 
-This project is built using React, TypeScript, Tailwind, and Firebase.
+This project is to practice the use of a back end service, in this case, Firebase.
 
-The main objective of this application is to recreate the concept of the popular game "Where's Waldo." Users are required to search for specific characters displayed in the sidebar within the given photo.
+The main objective of this web application is to recreate the concept of the popular game "Where's Waldo." Users are required to search for specific characters displayed in the sidebar within the given photo. Firebase is used to store the images and locations of all the characters.
 
-Unlike traditional styling methods like Sass, this project utilizes Tailwind CSS and incorporates the Flowbite framework to enhance the user interface. By combining these technologies, the app aims to provide a visually appealing and interactive experience for users.
+Unlike most of the other projects where I used Sass to style them, this project utilizes Tailwind CSS and incorporates the Flowbite UI component library to enhance the user interface. By combining these technologies, the app aims to provide a visually appealing and interactive experience for users.
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,18 +12,22 @@ export function Component() {
       </h1>
       <section className='my-4 md:text-lg grid gap-4 2xl:text-xl'>
         <p>
-          This project is built using React, TypeScript, Tailwind, and Firebase.
+          This project is to practice the use of a back end service, in this
+          case, Firebase.
         </p>
         <p>
-          The main objective of this application is to recreate the concept of
-          the popular game "Where's Waldo." Users are required to search for
+          The main objective of this web application is to recreate the concept
+          of the popular game "Where's Waldo." Users are required to search for
           specific characters displayed in the sidebar within the given photo.
+          Firebase is used to store the images and locations of all the
+          characters.
         </p>
         <p>
-          Unlike traditional styling methods like Sass, this project utilizes
-          Tailwind CSS and incorporates the Flowbite framework to enhance the
-          user interface. By combining these technologies, the app aims to
-          provide a visually appealing and interactive experience for users.
+          Unlike most of the other projects where I used Sass to style them,
+          this project utilizes Tailwind CSS and incorporates the Flowbite UI
+          component library to enhance the user interface. By combining these
+          technologies, the app aims to provide a visually appealing and
+          interactive experience for users.
         </p>
       </section>
     </div>
